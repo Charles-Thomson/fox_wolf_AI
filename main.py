050479962be596a48cd15556bb68804108d24d0d
Charles-PC <ct354@kent.ac.uk>
2022-06-28 20:49:02 +0100

    Implementing Dataclass approach- redundent removed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import setup as st
+import NewSetUp as setup
 import time
 from AI import fox_AI as fox_AI 
 from AI import wolf_AI as wolf_AI
+import foxAI as foxAI
 
 import tkinter as tk
 from tkinter import *
@@ -12,34 +14,27 @@ class main():
         self.node_size = node_size
         self.board_height = board_height
         self.board_width = board_width
-        self.alive_fox_locations, self.alive_wolf_locations, self.built_canvas = self.start_setup(parent,node_size,board_height,board_width)
-        # One fox support currently 
-        self.fox = self.alive_fox_locations[0]
+        self.built_canvas, self.foxs, self.wolfs = setup.main(parent,self.node_size,self.board_height,self.board_width)
         self.start_game()
-
-        #self.test_move_method()
-    def start_setup(self,parent,node_size,board_height,board_width):
-        alive_fox_locations, alive_wolf_locations, built_canvas = st.set_up(parent,node_size,board_height,board_width)
-        return alive_fox_locations, alive_wolf_locations, built_canvas
     
     # change to run game ? - this will be the main loop 
     def start_game(self):
-        
-        fox_AI.detect_wolfs_in_range_of_fox(self.alive_fox_locations,self.alive_wolf_locations,self.built_canvas,self.node_size)
-        wolf_AI_object = wolf_AI.wolf_AI(self.built_canvas,self.alive_wolf_locations,self.alive_fox_locations, self.node_size)
-        wolf_AI_object.detect_foxs_in_range_of_wolf()
+        foxAI.DetectWolfsInRange(self.foxs,self.wolfs,self.built_canvas,self.node_size)
+        #fox_AI.detect_wolfs_in_range_of_fox(self.alive_fox_locations,self.alive_wolf_locations,self.built_canvas,self.node_size)
+        #wolf_AI_object = wolf_AI.wolf_AI(self.built_canvas,self.alive_wolf_locations,self.alive_fox_locations, self.node_size)
+        #wolf_AI_object.detect_foxs_in_range_of_wolf()
 
-        self.built_canvas.after(5000, self.start_game)
+        #self.built_canvas.after(5000, self.start_game)
 
 
 
 
         # Do next  
-        # next - move over to data classes for both fox and wolf 
-        # currently creating new setup
-        # then - change the move for the fox , add in the sight range element perhaps ?
+        # continue with refactoring to use the dataclass approach
+        # currently working in new foxAI
         # 
-        #         look at logging instead of print /?
+        # 
+        #look at logging instead of print /?
 
 
 
